a23f3e87f839f39d7f4491dcc6c2c623349d7778
kamil <kamil.rahmatullin@gmail.com>
2025-05-04 20:28:37 +0300

    .rec precessing

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ import argparse
 from preprocessing.preprocessing import Preprocessor
 import utils
 import os
+import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--yolo_path', type=str, required=True)
     parser.add_argument('--dlib_path', type=str, required=True)
-    parser.add_argument('--images_path', type=str, required=True)
-    parser.add_argument('--save_path', type=str, required=True)
+    parser.add_argument('--rec_path', type=str, required=True)
+    parser.add_argument('--idx_path', type=str, required=True)
+    parser.add_argument('--base_path', type=str, required=True)
+    parser.add_argument('--proc_path', type=str, required=True)
     args = parser.parse_args()
 
-    os.makedirs(args.save_path, exist_ok=True)
+    os.makedirs(args.base_path, exist_ok=True)
+    os.makedirs(args.proc_path, exist_ok=True)
+
+    utils.write_rec_file(args.rec_path, args.idx_path, args.base_path)
 
     preprocessor = Preprocessor(args.yolo_path, args.dlib_path)
-    for i in os.listdir(args.images_path):
-        tensor = preprocessor.preprocess(os.path.join(args.images_path, i))
-        utils.save_img(tensor, os.path.join(args.save_path, i))
+    c = 0
+    for i in sorted(os.listdir(args.base_path)):
+        tensor = preprocessor.preprocess(args.base_path + "/" + i)
+        for face in tensor:
+            utils.save_img(face, args.proc_path + "/" + f'image_{c}.jpg')
+        c += 1
 
 if __name__ == '__main__':
     main()
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,20 @@
 import cv2
 import numpy as np
+import mxnet as mx
+import os
 
 def save_img(image, output_path):
-    cv2.imwrite(output_path, cv2.cvtColor(np.array(image).squeeze(0), cv2.COLOR_BGR2RGB))
+    cv2.imwrite(output_path, cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB))
+
+def write_rec_file(rec_path, idx_path, output_path):
+    data_iter = mx.image.ImageIter(
+        batch_size=1,
+        data_shape=(3, 112, 112),
+        path_imgrec=rec_path,
+        path_imgidx=idx_path,
+    )
+    data_iter.reset()
+    for i in range(10):
+        batch = data_iter.next()
+        data = batch.data[0].asnumpy().astype(np.uint8).squeeze(0).transpose((1, 2, 0))
+        save_img(data, output_path + f"/image_{i}.jpg")
