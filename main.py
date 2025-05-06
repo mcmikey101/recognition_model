027e97cff23087c6e4ef52bf55761d51a9d3f243
Камиль Рахматуллин <kamil.rahmatullin@gmail.com>
2025-05-06 09:26:44 +0300

    save

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import argparse
 from preprocessing.preprocessing import Preprocessor
 import utils
 import os
+from tqdm.auto import tqdm
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo_path', type=str, required=True)
-    parser.add_argument('--dlib_path', type=str, required=True)
-    parser.add_argument('--images_path', type=str, required=True)
+    parser.add_argument('--yolo', type=str, required=True)
+    parser.add_argument('--dlib', type=str, required=True)
+    parser.add_argument('--base_path', type=str, required=True)
     parser.add_argument('--save_path', type=str, required=True)
     args = parser.parse_args()
 
     os.makedirs(args.save_path, exist_ok=True)
 
-    preprocessor = Preprocessor(args.yolo_path, args.dlib_path)
-    for i in os.listdir(args.images_path):
-        tensor = preprocessor.preprocess(os.path.join(args.images_path, i))
-        utils.save_img(tensor, os.path.join(args.save_path, i))
+    preprocessor = Preprocessor(args.yolo, args.dlib)
+    for i in tqdm(os.listdir(args.base_path)):
+        img = preprocessor.preprocess(os.path.join(args.base_path, i))
+        utils.save_img(img, os.path.join(args.save_path, i))
 
 if __name__ == '__main__':
     main()
